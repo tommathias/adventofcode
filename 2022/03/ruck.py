2b6a2e0f11a22b18d41f6f95ead1b7da4d27f1fa
Tom Mathias <tommathiasdev@gmail.com>
2022-12-04 15:39:01 +0000

    part 1

---
+++ 2022/03/ruck.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+compartmentN = 2
+total = 0
+
+def getPriority(char):
+  if not char.isalpha():
+    raise('Can only prioritise letters')
+
+  cOrd = ord(char)
+  if(cOrd > 96): #lower
+    return cOrd-96
+  if(cOrd > 64): #upper
+    return cOrd-38
+  raise('You werent supposed to be able to get here you know')
+
+with open('input.txt') as f:
+  for line in f:
+    line = line.strip() #remove \n
+    #print(line)
+    #split line into n=2 compartments
+    itemCount = len(line)
+    compartmentSize = itemCount/compartmentN
+
+    bag = []
+    for i in range(compartmentN):
+      start = int(i*compartmentSize)
+      end = int(start+compartmentSize)
+      contents = line[start:end]
+      bag.append(contents)
+
+    for c in bag[0]:
+      if c in bag[1]:
+        total+=getPriority(c)
+        break
+
+print(total)
