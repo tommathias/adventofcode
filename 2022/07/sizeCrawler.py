7c246cf74831e2c2b587dea3ed0b5f94ee630747
Tom Mathias <tommathiasdev@gmail.com>
2023-05-17 17:18:29 +0100

    resolved clashing in aggregateSize()

--- 2022/07/sizeCrawler.py
+++ 2022/07/sizeCrawler.py
@@ -1,67 +1,47 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.10
 import re
-verbose = False
-
-# def updateFs(fileSystem, pwdStack):
-#   parent = None
-#   for dirName in pwdStack:
-#     if parent == None:
-#       parent = fileSystem['root']
-#       continue
-#     if not dirName in parent:
-#       parent[dirName] = {}
-#     parent = parent[dirName]
-#   return parent
+logLevel = 0
 
 def aggregateSize(results, pwdStack, fileSize):
+  #qualify names in results to prevent collisions
+  path = ''
   for dirName in pwdStack:
-    if dirName not in results:
-      results[dirName] = 0
-    results[dirName] += int(fileSize)
+    path = '/'.join([path, dirName])
+    if path not in results:
+      results[path] = 0
+    results[path] += int(fileSize)
 
 with open('input.txt') as f:
-  fileSystem = {}
+  fileSystem = { 'root': {} }
   pwdStack = []
-  #pwd = {}
   results = { 'root' : 0 }
 
   for line in f:
-    if verbose: print(f'line: {line}')
-    if verbose: print(f'pwdStack: {pwdStack}')
-    #if verbose: print(f'pwd: {pwd}')
-    #for python 3.8, so no match/case
-    if line[0] == '$':
-      cdMatch = re.match('^\$ cd (.*)$', line) # get cd arg into group
-      if cdMatch:
-        if verbose: print(f'cd: {cdMatch.group(1)}')
-        newDir = cdMatch.group(1)
-        if newDir =='..':
-          pwdStack.pop()
-          newDir = pwdStack[-1]
-        else:
-          if newDir == '/':
-            newDir = 'root'
-            pwdStack.append(newDir)
-            fileSystem['root'] = {}
-          else:
-            pwdStack.append(newDir)
-            #pwd = updateFs(fileSystem, pwdStack)
-      else:
-        if verbose: print('ls')
-    else:
-      # will be an ls output line
-      dirMatch = re.match('^dir (.*)$', line)
-      if dirMatch:
-        #pwd[dirMatch.group(1)] = {}
-        continue
-      fileMatch = re.match('^(\d.*) (.*)$', line)
-      fileSize = fileMatch.group(1)
-      fileName = fileMatch.group(2)
-      #pwd[fileName] = fileSize
-      aggregateSize(results, pwdStack, fileSize)
-    if verbose: print('=====')
+    if logLevel > 0: print(f'line: {line}')
+    if logLevel > 0: print(f'pwdStack: {pwdStack}')
+    match line.split(): # can match on structured data not just literals
+      case '$', 'cd', '/':
+        if logLevel > 1: print('cd /')
+        pwdStack = ['root']
+      case '$', 'cd', '..':
+        if logLevel > 1: print('cd ..')
+        pwdStack.pop()
+      case '$', 'cd', dir:
+        if logLevel > 1: print(f'cd {dir}')
+        pwdStack.append(dir)
+      case '$', 'ls':
+        if logLevel > 1: print('ls')
+      case 'dir', dir:
+        if logLevel > 1: print(f'dir: {dir}')
+      case fileSize, fileName:
+        if logLevel > 1: print(f'file: {fileName, fileSize}')
+        aggregateSize(results, pwdStack, fileSize)
+      case _:
+        raise('no line match')
+    if logLevel > 0: print(f'pwdStack: {pwdStack}')
+    if logLevel > 0: print('=====')
 
-#if verbose: print(f'fileSystem: {fileSystem}')
+if logLevel > 2: print(f'fileSystem: {fileSystem}')
 print(results)
 
 result = 0
