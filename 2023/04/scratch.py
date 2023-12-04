5fc5b3ecd39fcd7d39050eddcbc035878d61a40b
Tom Mathias <tommathiasdev@gmail.com>
2023-12-04 15:02:45 +0000

    part2

--- 2023/04/scratch.py
+++ 2023/04/scratch.py
@@ -7,9 +7,19 @@ def main():
   if (len(sys.argv) > 1): srcFile = sys.argv[1]
   if verbosityLevel > 0: print(f'srcFile: {srcFile}')
   totalPoints = 0
+  cards=[0]
+  totalCards = 0
+
+  def addCards(i, n):
+    nonlocal cards
+    if i > len(cards)-1: cards.append(0)
+    cards[i] += n
 
   with open(srcFile) as f:
-    for line in f:
+    for i, line in enumerate(f):
+      #add self to final cards
+      addCards(i, 1)
+      print(cards)
       #read winning numbers
       splitLine = line.split(':')[1].split('|')
       winners = splitLine[0].strip().split(' ')
@@ -24,12 +34,17 @@ def main():
           if verbosityLevel > 1: print(f'{number} is a match')
           matches+=1
       #calculate points: 2^n-1
-      if matches == 0: continue
-      cardPoints = 2**(matches-1)
-      if verbosityLevel > 1: print(f'{matches} matches and {cardPoints} points for this card')
-      totalPoints+=cardPoints
+      if matches > 0:
+        cardPoints = 2**(matches-1)
+        if verbosityLevel > 1: print(f'{matches} matches and {cardPoints} points for this card')
+        totalPoints+=cardPoints
+      #insert a card for each win
+      for j in range(1, matches+1):
+        addCards(i+j, cards[i])
+      totalCards+=cards[i]
+      print(totalCards)
 
-  print(totalPoints)
+  print(f'totalPoints: {totalPoints}, totalCards: {totalCards}')
   print('Thank you for playing Wing Commander')
 
 if __name__ == '__main__': main()
